7a0dfc70dc2f0bc38e68399dddf31f3fc01e24cb
ReflexTen <bulanov.roman4@gmail.com>
2023-03-01 22:13:01 +0500

    added section questions

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,15 @@
-// const use = 1
+'use strict'
+
+const buttonClose = document.querySelectorAll('.questions__item-close')
+const questionsText = document.querySelectorAll('.questions__item-text')
+
+buttonClose.forEach(el => {
+  el.addEventListener('click', () => {
+    const text = el.closest('.questions__item').children[1]
+    console.log(text)
+    // .children('.questions__item-text')
+    text.classList.toggle('active')
+  })
+})
+
+// buttonClose.classList.remove()
